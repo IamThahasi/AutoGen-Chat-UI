a03cf46aecf568b02fae69bd542766d976755ea3
IamThahasi <thahaseenakp@gmail.com>
2024-12-03 10:46:32 +1100

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ selected_model = None
 selected_key = None
 with st.sidebar:
     st.header("OpenAI Configuration")
-    selected_model = st.selectbox("Model", ['gpt-3.5-turbo', 'gpt-4'], index=1)
+    selected_model = st.selectbox("Model", ['gpt-3.5-turbo', 'gpt-4', 'llama-3.2-3b-instruct'], index=1)
     selected_key = st.text_input("API Key", type="password")
 
 with st.container():
